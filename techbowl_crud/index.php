7cb5707e9f86b991bfcf4a1c60281ff629277c0b
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-02-04 23:32:38 +0100

    read

--- techbowl_crud/index.php
+++ techbowl_crud/index.php
@@ -1,3 +1,10 @@
+<?php
+    require_once 'includes/db.php';
+    $sql = "SELECT * FROM students";
+    $results = mysqli_query($conn, $sql);
+    $records = mysqli_num_rows($results);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <?php include 'partial/head.php'; ?>
@@ -17,17 +24,25 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row"></th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>
-                        <a href="/" class="btn btn-primary">EDIT</a>
-                        <a href="/" class="btn btn-danger">DELETE</a>
-                    </td>
-                </tr>
+                <?php
+                    if (!empty($records)) {
+                        while ($row = mysqli_fetch_assoc($results)) {
+                            ?>
+                                <tr>
+                                    <th scope="row"><?php echo $row['id']; ?></th>
+                                    <td><?php echo $row['first_name'].' '.$row['last_name']; ?></td>
+                                    <td><?php echo $row['gender']; ?></td>
+                                    <td><?php echo $row['email']; ?></td>
+                                    <td><?php echo $row['course']; ?></td>
+                                    <td>
+                                        <a href="/" class="btn btn-primary">EDIT</a>
+                                        <a href="/" class="btn btn-danger">DELETE</a>
+                                    </td>
+                                </tr>
+                            <?php
+                        }
+                    }
+                ?>
             </tbody>
         </table>
     </div>  
